1bb38027856b5a880f2d2c7ae7c0e234331c767f
dylanclements7 <157054595+dylanclements7@users.noreply.github.com>
2024-02-29 18:21:10 -0500

    Add files via upload

---
+++ src/DinnerParty.java
@@ -0,0 +1,12 @@
+public class DinnerParty extends Party {
+    int DinnerChoice;
+    //initializes variable
+    public int get_DinnerChoice(){
+        return DinnerChoice;
+    }
+//gets dinner choice
+    public void set_DinnerChoice(int new_DinnerChoice){
+        this.DinnerChoice = new_DinnerChoice;
+    }
+    //sets dinner choice
+}
